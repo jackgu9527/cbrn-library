42dd00be7ac08108bbe0befbb84a6558dfcec179
jackgu9527 <pgh78940@gmail.com>
2026-03-27 21:25:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -456,7 +456,7 @@ if 'logged_in' not in st.session_state:
 
     with tab2:
         st.subheader("班隊註冊", help="""
-:blue[**【班隊註冊】**]
+:blue[**【班隊註冊】**]  
 :yellow[**【所屬中隊】**]：請填寫報到的中隊
 :yellow[**【班隊全銜】**]：請填寫受訓的名稱
 :yellow[**【設定登入帳號】**]：設定登錄頁面登錄帳號
