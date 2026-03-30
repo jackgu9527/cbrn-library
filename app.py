4a8ee6a5ecbd9d984ca511805453d8b2366d09c8
jackgu9527 <pgh78940@gmail.com>
2026-03-30 11:06:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -494,12 +494,12 @@ with st.sidebar:
             st.session_state['current_sq'] = st.selectbox("選擇中隊", sq_list, key="global_sq_selector", label_visibility="collapsed")
         else:
             st.session_state['current_sq'] = sq_list[0]
-            st.markdown(f"**管轄中隊：** `{st.session_state['current_sq']}`")
+            st.markdown(f"**中隊：** `{st.session_state['current_sq']}`")
         menu_options = ["🏠 首頁", "👥 帳號管理", "📤 借閱審核", "📥 歸還審核", "💬 回報專區", "📊 準則現況", "🔍 綜合查詢", "🗂️ 操作紀錄"]
         if str(st.session_state.squadron).strip() == '大隊部': menu_options.insert(2, "⚙️ 系統管理")
     else:
         st.session_state['current_sq'] = st.session_state.squadron
-        st.markdown(f"📍 **所屬中隊：** `{st.session_state['current_sq']}`")
+        st.markdown(f"**中隊：** `{st.session_state['current_sq']}`")
         menu_options = ["🏠 首頁", "📤 準則借閱", "🏷️ 序號登載", "📥 準則歸還", "💬 回報專區", "🔍 綜合查詢", "🚗 車輛登載"]
         
     # 3. 功能導覽 (上下加上自訂的細分隔線)
@@ -1018,7 +1018,7 @@ try:
                 st.success("✨ 您名下目前沒有需要歸還的準則！")
 
         elif menu == "⚙️ 系統管理" and st.session_state.role == 'L1' and str(st.session_state.squadron).strip() == '大隊部':
-            st.header("👑 系統管理員模式", help="系統最高權限可查看與修改所有人員權限。")
+            st.header("👑 系統管理", help="系統最高權限可查看與修改所有人員權限。")
             # 👇 --- 新增這個對話框函數 --- 👇
             @st.dialog("⚙️ 編輯與管理帳號")
             def edit_user_dialog(user_row):
